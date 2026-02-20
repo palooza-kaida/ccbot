eb8d759f996a5e0ace4fc00975bd9fe3aa3175dc
kaida <kaida.palooza@gmail.com>
2026-02-20 20:38:49 +0700

    chore: add eslint, prettier, husky, editorconfig

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+
+export default defineConfig([
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  prettierConfig,
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: {
+      "prettier/prettier": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+  },
+  {
+    files: ["src/utils/log.ts"],
+    rules: {
+      "no-console": "off",
+    },
+  },
+  globalIgnores(["dist/", "node_modules/", "public/", "*.js", "!eslint.config.js"]),
+]);
